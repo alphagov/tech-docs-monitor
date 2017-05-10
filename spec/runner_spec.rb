d657cfb6d75938caeb8d014e8e9ce9f5fdd0b8c2
Tijmen Brommet <tijmen@gmail.com>
2017-05-10 17:20:23 +0100

    Allow per-channel notifications
    This will make the docs donkey complain to the channel specified in
    `owner_slack` on the page.

--- spec/runner_spec.rb
+++ spec/runner_spec.rb
@@ -12,11 +12,24 @@ end
 RSpec.describe Runner do
   it "generates the correct message" do
     VCR.use_cassette "fresh" do
-      payload = Runner.new.message_payload
+      payloads = Runner.new.message_payloads
 
-      expect(payload[:text]).to match(
-        "<https://docs.publishing.service.gov.uk/manual/alerts/asset-master-attachment-processing.html|asset master attachment processing> should be reviewed now by #2ndline"
-      )
+      expect(payloads).to match([
+        {
+          username: "Donkey of Docs",
+          icon_emoji: ":donkeywork:",
+          text: "Hello :wave:, this is your friendly donkey of documentation.\n\n- <https://docs.publishing.service.gov.uk/manual/alerts/asset-master-attachment-processing.html|asset master attachment processing> should be reviewed now\n",
+          mrkdwn: true,
+          channel: "#2ndline",
+        },
+        {
+          username: "Donkey of Docs",
+          icon_emoji: ":donkeywork:",
+          text: "Hello :wave:, this is your friendly donkey of documentation.\n\n- <https://docs.publishing.service.gov.uk/manual/ab-testing.html|Run an A/B test> should be reviewed before 2017-04-14\n",
+          mrkdwn: true,
+          channel: "#taxonomy",
+        }
+      ])
     end
   end
 end
