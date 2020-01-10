f4223a3a4e2995001afd26e1646779d850a16600
Ollie <oliver.chalk@digital.cabinet-office.gov.uk>
2020-01-10 10:02:37 +0000

    No posting on pull requests

--- travis.sh
+++ travis.sh
@@ -7,6 +7,11 @@ set -eu
 bundle check || bundle install
 
 case "$TRAVIS_EVENT_TYPE" in
+  pull_request)
+    echo "Running tests and not posting anywhere"
+    bundle exec rspec
+    REALLY_POST_TO_SLACK="0" bundle exec rake notify:expired
+    ;;
   pr|pull_request|push)
     echo "Running tests and posting to #bot-testing"
     bundle exec rspec
