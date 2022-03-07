9a36e8e0d60a45beacc88731abd64fbf5e6d04e4
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2022-03-07 16:12:20 +0000

    Add VCR RSpec metadata configuration
    Needed to make `vcr: true` work.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ require "vcr"
 VCR.configure do |config|
   config.cassette_library_dir = "fixtures/vcr_cassettes"
   config.hook_into :webmock
+  config.configure_rspec_metadata!
 end
 
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
